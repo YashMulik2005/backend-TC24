5fb9a13b6b1d619a7dbfc49a2f4cacc5c242d2bd
Yash aghane <yashaghane21@gmail.com>
2024-02-17 16:01:17 +0530

    api change

--- controller/ProjectController.js
+++ controller/ProjectController.js
@@ -15,10 +15,9 @@ const getOneproject = async (req, res) => {
     const { project_id } = req.body;
     const data = await ProjectModel.find({ _id: project_id });
     return res.status(200).json({
-        data: {
             status: true,
             data: data
-        }
+        
     })
 }
 
